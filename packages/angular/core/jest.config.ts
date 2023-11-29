2cc12e435150f049f565d224ebacd46f0e695b91
Lucas GILHARD <l.gilhard@gmail.com>
2023-11-29 20:09:59 +0100

    migrate @omni/core

--- packages/angular/core/jest.config.ts
+++ packages/angular/core/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: '@omni/core',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/packages/core',
+  coverageDirectory: '../../../coverage/packages/angular/core',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
