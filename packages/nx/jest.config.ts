443c8f0dd49687aefc4ccacb274b079f82a6e717
Lucas GILHARD <l.gilhard@gmail.com>
2023-11-30 00:20:11 +0100

    fix dependencies

--- packages/nx/jest.config.ts
+++ packages/nx/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: '@omni/engine/nx',
+  displayName: '@omni/nx',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
